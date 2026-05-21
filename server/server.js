ef1ae9a5fa55a357cd63d0691b4990be2202cf92
Arizal Arkan <amonkucingsakti@gmail.com>
2026-05-21 16:16:00 +0700

    fix: server listen on 0.0.0.0 for Fly.io deployment

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import connectDB from "./config/db.js";
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
   });
 });
